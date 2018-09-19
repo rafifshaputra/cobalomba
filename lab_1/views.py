8493c4a5ba23bf453fb3ad937941702c1ec4c188
Rafif Shaputra <rafifshaputra25@gmail.com>
2018-09-19 10:06:39 +0700

    Edit New Page 2

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Album
@@ -8,12 +9,10 @@ mhs_name = 'Rafif Iqbal Shaputra'
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums' : all_albums,
-
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'music/index.html', context)
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for Album id " + str(album_id) + "</h2>")
